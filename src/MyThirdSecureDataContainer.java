4820ddca2b990917aca48255951b975826ab1e4e
anexus18 <galatoloalessio@gmail.com>
2018-11-11 20:21:18 +0100

    completed third implementation, missing null check

--- src/MyThirdSecureDataContainer.java
+++ src/MyThirdSecureDataContainer.java
@@ -1,31 +1,10 @@
+import javafx.util.Pair;
+
 import java.util.*;
 
 public class MyThirdSecureDataContainer<E> implements SecureDataContainer<E>{
-
-    private Hashtable<Credential, List<E>> table;
-
-    private class Credential{
-        private String id;
-        private String pass;
-
-        public Credential(String id, String pass){
-            if(id == null || pass == null)
-                throw new NullPointerException("id or pass are null");
-            this.id = id;
-            this.pass = pass;
-        }
-
-        @Override
-        public int hashCode() {
-            return id.hashCode();
-        }
-
-        @Override
-        public boolean equals(Object obj) {
-            Credential c = (Credential) obj;
-            return (c.id.equals(id) && c.pass.equals(pass));
-        }
-    }
+    //
+    private Hashtable<String, Pair<String, List<E>>> table;
 
     public MyThirdSecureDataContainer(){
         table = new Hashtable<>();
@@ -35,42 +14,122 @@ public class MyThirdSecureDataContainer<E> implements SecureDataContainer<E>{
     public void createUser(String Id, String passw) throws UserTakenException {
         if(Id == null || passw == null)
             throw new NullPointerException();
-        table.put(new Credential(Id, passw), new ArrayList<>());
+        if(table.get(Id) == null)
+            table.put(Id, new Pair<>(passw, new ArrayList<>()));
+        else
+            throw new UserTakenException();
 
     }
 
     @Override
     public int getSize(String Owner, String passw) throws IdNotFoundException, UnauthorizedException {
-        return 0;
+        Pair<String, List<E>> pair = table.get(Owner);
+        if(pair == null)
+            throw new IdNotFoundException();
+        if(pair.getKey().equals(passw))
+            return pair.getValue().size();
+        else
+            throw new UnauthorizedException();
+
     }
 
     @Override
     public boolean put(String Owner, String passw, E data) throws UnauthorizedException, IdNotFoundException {
-        return false;
+        Pair<String, List<E>> pair = table.get(Owner);
+        if(pair == null)
+            throw new IdNotFoundException();
+        if(pair.getKey().equals(passw)) {
+            pair.getValue().add(data);
+            return true;
+        }
+        else
+            throw new UnauthorizedException();
     }
 
     @Override
     public E get(String Owner, String passw, E data) throws UnauthorizedException, IdNotFoundException {
-        return null;
+        Pair<String, List<E>> pair = table.get(Owner);
+        if(pair == null)
+            throw new IdNotFoundException();
+        if(pair.getKey().equals(passw)){
+            return pair.getValue().get(pair.getValue().indexOf(data));
+        }
+        else
+            throw new UnauthorizedException();
     }
 
     @Override
     public E remove(String Owner, String passw, E data) throws UnauthorizedException, IdNotFoundException {
-        return null;
+        Pair<String, List<E>> pair = table.get(Owner);
+        if(pair == null)
+            throw new IdNotFoundException();
+        if(pair.getKey().equals(passw)){
+            pair.getValue().remove(data);
+            return data;
+        }
+        else
+            throw new UnauthorizedException();
     }
 
     @Override
     public void copy(String Owner, String passw, E data) throws UnauthorizedException, IdNotFoundException {
-
+        Pair<String, List<E>> pair = table.get(Owner);
+        if(pair == null)
+            throw new IdNotFoundException();
+        if(pair.getKey().equals(passw))
+            pair.getValue().add(data);
+        else
+            throw new UnauthorizedException();
     }
 
     @Override
     public void share(String Owner, String passw, String Other, E data) throws UnauthorizedException, IdNotFoundException {
+        Pair<String, List<E>> ownerPair = table.get(Owner);
+        Pair<String, List<E>> otherPair = table.get(Other);
 
+        if(ownerPair == null || otherPair == null)
+            throw new IdNotFoundException();
+
+        if(ownerPair.getKey().equals(passw))
+            otherPair.getValue().add(data);
+        else
+            throw new UnauthorizedException();
     }
 
     @Override
     public Iterator<E> getIterator(String Owner, String passw) throws UnauthorizedException, IdNotFoundException {
-        return null;
+        Pair<String, List<E>> pair = table.get(Owner);
+        if(pair == null)
+            throw new IdNotFoundException();
+        if(pair.getKey().equals(passw))
+            return pair.getValue().iterator();
+        else
+            throw new UnauthorizedException();
     }
+
+
+//    private class Credential{
+//        private String id;
+//        private String pass;
+//
+//        private Credential(String id, String pass){
+//            if(id == null || pass == null)
+//                throw new NullPointerException("id or pass are null");
+//            this.id = id;
+//            this.pass = pass;
+//        }
+//
+//        @Override
+//        public int hashCode() {
+//            return id.hashCode();
+//        }
+//
+//        @Override
+//        public boolean equals(Object obj) {
+//            Credential c = (Credential) obj;
+//            return (c.id.equals(id) && c.pass.equals(pass));
+//        }
+//    }
+
+
 }
